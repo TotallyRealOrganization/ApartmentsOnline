9bc06750b40ae21a886477864d61942d24766a10
dan39radke <dan39radke@gmail.com>
2020-08-21 16:01:19 -0400

    adding services and controllers

--- src/main/java/com/apartmentsonline/Controllers/ApartmentController.java
+++ src/main/java/com/apartmentsonline/Controllers/ApartmentController.java
@@ -1,4 +1,7 @@
 package com.apartmentsonline.Controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApartmentController {
 }
---
+++ src/main/java/com/apartmentsonline/Services/ApartmentService.java
@@ -0,0 +1,4 @@
+package com.apartmentsonline.Services;
+
+public class ApartmentService {
+}

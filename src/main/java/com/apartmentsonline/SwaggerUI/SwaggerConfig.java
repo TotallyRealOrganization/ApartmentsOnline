06e34363cb92c1e24b9ec81bb22b47edb1f2a2b5
dan39radke <dan39radke@gmail.com>
2020-08-24 22:13:28 -0400

    added swagger except not finished

---
+++ src/main/java/com/apartmentsonline/SwaggerUI/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.apartmentsonline.SwaggerUI;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
+
+   /* @Override
+   *//* public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }*/
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/docApi/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/docApi/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/docApi/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/docApi/swagger-resources", "/swagger-resources");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docApi/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/docApi/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}

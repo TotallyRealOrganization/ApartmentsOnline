1e2c9b9b9959153eb6429ecf146431ad9f8cee1f
dan39radke <dan39radke@gmail.com>
2020-08-25 02:19:13 -0400

    user service/controller

--- src/main/java/com/apartmentsonline/Controllers/UserController.java
+++ src/main/java/com/apartmentsonline/Controllers/UserController.java
@@ -1,7 +1,41 @@
 package com.apartmentsonline.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.apartmentsonline.Services.UserService;
+import com.apartmentsonline.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.jws.soap.SOAPBinding;
+import java.awt.*;
+
+@Controller
+@RequestMapping(path = "/user")
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // get user by id
+    @GetMapping(path = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<User> getUserById(@PathVariable("userId") String id) {
+        User user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> saveNewUser(@RequestBody User user) {
+        User u = userService.saveNewUser(user);
+        return ResponseEntity.ok().body("New User created: Id = " + u.getId());
+    }
+
 }
--- src/main/java/com/apartmentsonline/Services/UserService.java
+++ src/main/java/com/apartmentsonline/Services/UserService.java
@@ -1,4 +1,34 @@
 package com.apartmentsonline.Services;
 
+import com.apartmentsonline.DAO.UserDAO;
+import com.apartmentsonline.models.User;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
 public class UserService {
+
+    private UserDAO userDAO;
+
+    @Autowired
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public User saveNewUser(User user) {
+        return userDAO.saveNewUser(user);
+    }
+
+    public User getUserById(String id) {
+        return userDAO.getUserById(id);
+    }
+
+    public boolean removeUser(User u) {
+       return userDAO.removeUser(u);
+    }
 }
